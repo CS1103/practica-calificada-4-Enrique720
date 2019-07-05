5fa14113c8b07b04797e7c12dc95192385eabf4b
Enrique Sobrados <39936196+Enrique720@users.noreply.github.com>
2019-07-05 12:51:51 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
+#include <vector>
+#include <thread>
+using namespace std;
+template <typename T>
+void buscar(vector<T> array,int num_thread,T*valor ,int id){
+    int limitesup = (id+1)*(array.size())/num_thread;
+    int limiteinf =  (id)*(array.size())/num_thread;
+    for(int i = limiteinf; i < limitesup; i++){
+        if(array[i]>*valor)
+              *valor = array[i];
+    }
+}
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+template  <typename T>
+T buscar_mayor(vector<T>array,int num_threads){
+    auto retorno = new T(0);
+    vector<thread> hilos;
+    for(int i = 0; i < num_threads; i++){
+         hilos.emplace_back(ref(buscar<T>),ref(array),ref(num_threads),retorno,ref(i));
+    }
+    for(int i = 0; i < num_threads; i++){
+        hilos[i].join();
+    }
+    return *retorno;
+}
+
+int main(){
+    vector<int> a;
+    a.push_back(5);
+    a.push_back(6);
+    a.push_back(7);
+    a.push_back(1);
+
+    cout << buscar_mayor(a,2);
+
 }
